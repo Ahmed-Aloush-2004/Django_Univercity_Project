388e747ab0f124406b554e9f5ec88c3c92555141
Nourankandar <147334965+Nourankandar@users.noreply.github.com>
2026-05-18 11:56:58 +0300

    little change for hosts

--- my_site/settings.py
+++ my_site/settings.py
@@ -17,7 +17,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []  # Add your allowed hosts in production
+ALLOWED_HOSTS = ['127.0.0.1',     
+    'localhost',]  
 
 # Application definition
 INSTALLED_APPS = [
